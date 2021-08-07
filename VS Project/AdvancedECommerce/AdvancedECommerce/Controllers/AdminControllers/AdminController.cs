99b48ce141d5d46fc9fba15bf271165b1b1630b4
Born0 <bornoahmed2@gmail.com>
2021-08-07 19:42:32 +0600

    Problem case img added

---
+++ VS Project/AdvancedECommerce/AdvancedECommerce/Controllers/AdminControllers/AdminController.cs
@@ -0,0 +1,40 @@
+﻿using AdvancedECommerce.Repository;
+using AdvanceECommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdvancedECommerce.Controllers.AdminControllers
+{
+    public class AdminController : Controller
+    {
+        private AdminRepository adminRepository = new AdminRepository();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            List<Admin> admins = adminRepository.GetAll();
+            return View(admins);
+        }
+
+        [HttpGet]
+        public ActionResult AddAdmin()
+        {
+            return View();
+        }
+
+        public ActionResult AddAdmin(Admin admin)
+        {
+            if(ModelState.IsValid)
+            {
+                adminRepository.Insert(admin);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction("AddAdmin");
+            }
+        }
+    }
+}
